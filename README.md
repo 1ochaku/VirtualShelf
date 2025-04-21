70f2f340f3296eab2df7892b7072ffbccbd662f6
Punyo Sunya <sunya22839@example.com>
2025-04-21 20:02:43 +0530

    updated readme, server.js

--- README.md
+++ README.md
@@ -3,3 +3,28 @@
 This is a simple React App (Backend functionality to be added) to track the borrowed and lent items.
 
 Currently, it has add item, delete item, and edit item features.
+
+## To Run The App In Your Machine
+
+1. First clone the repo
+
+```bash
+git clone https://github.com/1ochaku/VirtualShelf.git
+cd Virtual Shelf
+```
+
+2. Navigate to frontend
+
+```bash
+cd frontend
+npm intall
+npm start
+```
+
+3. Navigate to backend in another terminal
+
+```bash
+cd backend
+npm intall
+npm run dev
+```
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello, Express.js Server!</h1>');
+app.get('/getItems', (req, res) => {
+    res.send('<h1>Here are the list of Items.</h1>');
+});
+
+app.post('/addItems', (req, res) => {
+    // to add the adding of items logic here
+});
+
+app.patch('/updateItems', (req, res) => {
+    // to add the updation  of items logic here
+});
+
+app.delete('/deleteItems', (req, res) => {
+    // to add the deletion of items logic here
 });
 
 app.listen(port, () => {
