6fbe7dd018c5882de319f44d68ebd73edc5206f8
Punyo Sunya <sunya22839@example.com>
2025-04-21 20:22:04 +0530

    added the mongodb url

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+require('dotenv').config();
+
+const mongoURL = process.env.MONGO_URL;
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
 app.get('/getItems', (req, res) => {
     res.send('<h1>Here are the list of Items.</h1>');
 });
